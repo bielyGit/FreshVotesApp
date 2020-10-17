3b6d6391e85a8e0116b5decef0271a83626e3223
bielyGit <biely@o2.pl>
2020-10-17 13:50:46 +0200

    working with csrf

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,33 +1,42 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("biely")
-                .password("123pwd")
+                .password(getPasswordEncoder().encode("123pwd"))
                 .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
-                .anyRequest().hasRole("USER").and()
+                    .antMatchers("/").permitAll()
+                    .anyRequest().hasRole("USER").and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/dashboard")
+                    .permitAll()
+                    .and()
                 .logout()
-                .logoutUrl("/logout")
-                .permitAll();
+                    .logoutUrl("/logout")
+                    .permitAll();
     }
 }
--- src/main/java/com/freshvotes/web/DashboardController.java
+++ src/main/java/com/freshvotes/web/DashboardController.java
@@ -1,13 +1,22 @@
 package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DashboardController {
+
     @RequestMapping(value ="/", method = RequestMethod.GET)
     public String rootView(){
         return "index";
     }
+
+    @GetMapping("/dashboard")
+    public String dashboardView(){
+        return "dashboard";
+    }
+
+
 }
